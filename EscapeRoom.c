3fa4fbf1a95eca8045547fd11d9f67ccd3deda55
JoanBonell <joanbonellruiz@gmail.com>
2022-11-30 12:31:37 +0100

    modificat:        EscapeRoom.c 	fitxer nou:       Makefile 	modificat:        escaperoom 	modificat:        jugador.c

--- EscapeRoom.c
+++ EscapeRoom.c
@@ -6,39 +6,57 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-void crear_jugadores(int n_jugadores, pid_t *pid[]);
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+void crear_jugadores(int n_jugadores, pid_t **pids);
 int main(int argc, char *argv[]){
+    char s[100];
+    //Initialize *argv[] to ./jugador 1 2 aaaa
+    
     /*  Per a fer servir el programa, l'usuari hauria de fer:
         ./sala arg1 arg2 arg3
         Per això, ens assegurarem que l'usuari ens passi 3 arguments
         de la següent manera, si no, atuarem directament el programa.*/
-    if(argc <= 3){
-        printf("Has de posar com a mínim 3 arguments\n");
-        return 0;
+        //sprintf(s, "%sOh no!The pokemon broke free.%s\n", KBLU,KNRM);
+    if(argc < 3){
+        //sprintf(s, "Has de posar com a mínim 3 arguments\n", KRED, KNRM);
+        perror("Error, mínim 3 arguments.\n");
+        return EXIT_FAILURE;
     }
-    
-
-    /*  argv[2] = número de segons que volem que duri la partida com a màxim,
+    /*  argv[1] = número de segons que volem que duri la partida com a màxim,
         passarem aquest string a int i ho guardarem a 'num_segons_partida'*/
     int num_segons_partida = atoi(argv[1]); // Passem el string argv[1] a int i ho guardem a 'num_segons_partida'.
 
-    int n_jugadores = atoi(argv[2]);
-    pid_t * pids = malloc(sizeof(pid_t) * n_jugadores);
-
-    for(int i = 0; i < n_jugadores; i++){
-        pids[i] = fork();
-        switch (pids[i])
+    int n_jugadores = atoi(argv[2]); // Passem el string argv[2] a int i ho guardem a 'n_jugadores'.
+    pid_t * pids = malloc(sizeof(pid_t) * n_jugadores); // Reservem memòria per a guardar els pid dels jugadors.
+    //pipe(fd1); // Creem el pipe per a comunicar el pare amb el fill.
+    crear_jugadores(n_jugadores, &pids); // Creem els jugadors.
+    /*for(int i = 0; i < n_jugadores; i++){
+        pids[i] = fork(); // Creem un nou procés per a cada jugador.
+        
+        switch (pids[i]) // Comprovem si s'ha creat correctament.
         {
-        case 0:
+        case 0: // Si s'ha creat correctament, el procés fill executarà el programa jugador.
             printf("EscapeRoom[%d] ha entrat el jugador[%d]\n", getppid(), getpid());
+            
+            //dup2(, );
             //dup2
+            //execl("./jugador", argv[3]);
+            //dup2(fd1, fd2);
             //exec("jugador", NULL);
             break;
         case -1:
             perror("Error fork() - pid \n");
             break;
         }  
-    }
+    }*/
 
     //Create a pipe
     int fd[2];
@@ -51,6 +69,7 @@ int main(int argc, char *argv[]){
     // llegir de la pipe
     //Read from the pipe
     char read_msg[80];
+    execl("./jugador", argv[3]);
     read(fd[0], read_msg, sizeof(read_msg));
     printf("read %s",read_msg);
     //Write to the pipe
@@ -79,3 +98,27 @@ int main(int argc, char *argv[]){
 
 
 
+void crear_jugadores(int n_jugadores, pid_t **pids){
+    char s[100];
+    for(int i = 0; i < n_jugadores; i++){
+        *pids[i] = fork(); // Creem un nou procés per a cada jugador.
+        
+        switch (*pids[i]) // Comprovem si s'ha creat correctament.
+        {
+        case 0: // Si s'ha creat correctament, el procés fill executarà el programa jugador.
+            //setcolor(KBLU);
+            // sprintf(s, "%sOh no!The pokemon broke free.%s\n", KBLU,KNRM);
+            sprintf(s, "EscapeRoom[%d] ha entrat el jugador[%d]\n", getppid(), getpid(), KBLU, KNRM);
+            
+            //dup2(, );
+            //dup2
+            //execl("./jugador", argv[3]);
+            //dup2(fd1, fd2);
+            //exec("jugador", NULL);
+            break;
+        case -1:
+            perror("Error fork() - pid \n");
+            break;
+        }  
+    }
+}
